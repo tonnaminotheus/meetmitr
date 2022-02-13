c55d655d4584042831d84a42d38b73219c6ad13c
NAEM <tonnam_t@windowslive.com>
2022-02-13 22:30:50 +0700

    Branch Fusion

---
+++ frontend/src/screen/ChatLeft.js
@@ -0,0 +1,10 @@
+import naem from "./../asset/naemblack.jpg";
+const ChatLeft = (props) => {
+  return (
+    <div className="styles">
+      <img src={naem} />
+    </div>
+  );
+};
+
+export default ChatLeft;
