bbe0b3229ef98faa6219944103c4a08438ced242
oJadeo <jade.jiravate@gmail.com>
2022-02-16 02:12:48 +0700

    confirm api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import JoinEventDetail from "./screens/JoinEventDetail";
-import Register from "./screens/Register";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Register />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
