0e989afc05ef32ba4408e3d57f9dcfdfa2ca9742
Putt2020 <6230067421@student.chula.ac.th>
2022-03-01 22:03:33 +0700

    sprint2 final commit

--- frontend/src/globalApi.js
+++ frontend/src/globalApi.js
@@ -1,9 +1,10 @@
-const url = "http://54.172.159.163:8080";
+const url = "http://54.152.107.122:8080";
 const globalApi = {
   register: `${url}/api/v1/register`,
   login: `${url}/api/v1/login`,
   activate: `${url}/api/v1/activate/`, //+:activStr
   joinEvent: `${url}/api/v1/event/join/`, //+:eventId v2022.01 (Auth)
+  unJoinEvent: `${url}/api/v1/event/unjoin/`, //+:eventId v2022.01 (Auth)
   tagsEvent: `${url}/api/v1/event/tags/`,
   updateEvent: `${url}/api/v1/event/update/`, //+:eventId v2022.01 (Auth)
   eventDescription: `${url}/api/v1/event/descriptions/`, //+:eventId v2022.01
