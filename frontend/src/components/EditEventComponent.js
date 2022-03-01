848ee037e373e355c54fc33e9f24682862843efb
AfteRRaIN-Sys <70005493+AfteRRaIN-Sys@users.noreply.github.com>
2022-03-01 09:17:47 +0700

    little change

--- frontend/src/components/EditEventComponent.js
+++ frontend/src/components/EditEventComponent.js
@@ -9,12 +9,12 @@ const EditEventComponent=(props)=>{
     const {state} = useLocation();
     // console.log("eventID :"+eventID)  
     
-    const [img_src, setImgSrc] = useState("")
+    const [img_path,setPicURL] = useState("")
     
     return (
         <div className="create-event-container">
             <CreateEventPicComponent setImgSrc={setImgSrc}/>
-            {state&&<CreateEventInfoComponent eventID={state.eventId} img_src={img_src}/>}
+            {state&&<CreateEventInfoComponent eventID={state.eventId} img_src={img_path}/>}
             {!state&&<CreateEventInfoComponent/>}
         </div>
     );
