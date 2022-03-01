a0125bd932bd1f6b6f9c8b872c43cb00bfc5a0c6
Supawich Puengdang <supawich@gmail.com>
2022-03-01 19:23:46 +0700

    Edit imgURL to list of string

--- backend/app/requests/eventRequests.go
+++ backend/app/requests/eventRequests.go
@@ -14,29 +14,29 @@ type EventJoinReq struct {
 }
 
 type CreateEventReq struct {
-	Name           string `json:"name" binding:"required"`
-	Description    string `json:"description" binding:"required"`
-	Address        string `json:"address" binding:"required"`
-	Province       string `json:"province" binding:"required"`
-	ImagUrl        string `json:"imagUrl"`
-	StartTime      string `json:"startTime" binding:"required"`
-	EndTime        string `json:"endTime" binding:"required"`
-	Tags           []int  `json:"tags"`
-	Onsite         bool   `json:"onsite" binding:"required"`
-	MaxParticipant int    `json:"maxParticipant" binding:"required"`
-	Price          *int   `json:"price" binding:"required"`
+	Name           string   `json:"name" binding:"required"`
+	Description    string   `json:"description" binding:"required"`
+	Address        string   `json:"address" binding:"required"`
+	Province       string   `json:"province" binding:"required"`
+	ImagUrl        []string `json:"imagUrl"`
+	StartTime      string   `json:"startTime" binding:"required"`
+	EndTime        string   `json:"endTime" binding:"required"`
+	Tags           []int    `json:"tags"`
+	Onsite         bool     `json:"onsite" binding:"required"`
+	MaxParticipant int      `json:"maxParticipant" binding:"required"`
+	Price          *int     `json:"price" binding:"required"`
 }
 
 type UpdateEventReq struct {
-	Name           string `json:"name"`
-	Description    string `json:"description"`
-	Address        string `json:"address"`
-	Province       string `json:"province"`
-	ImagUrl        string `json:"imagUrl"`
-	StartTime      string `json:"startTime"`
-	EndTime        string `json:"endTime"`
-	Tags           []int  `json:"tags"`
-	Onsite         *bool  `json:"onsite"`
-	MaxParticipant *int   `json:"maxParticipant"`
-	Price          *int   `json:"price"`
+	Name           string   `json:"name"`
+	Description    string   `json:"description"`
+	Address        string   `json:"address"`
+	Province       string   `json:"province"`
+	ImagUrl        []string `json:"imagUrl"`
+	StartTime      string   `json:"startTime"`
+	EndTime        string   `json:"endTime"`
+	Tags           []int    `json:"tags"`
+	Onsite         *bool    `json:"onsite"`
+	MaxParticipant *int     `json:"maxParticipant"`
+	Price          *int     `json:"price"`
 }
