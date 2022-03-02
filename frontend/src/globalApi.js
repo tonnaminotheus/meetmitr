a1acbbb4bfd4d41bd4b6751370c553323c7c07e7
NAEM <tonnam_t@windowslive.com>
2022-03-02 13:45:09 +0700

    for pull

--- frontend/src/globalApi.js
+++ frontend/src/globalApi.js
@@ -1,5 +1,4 @@
-const url = "http://52.91.79.60:8080";
-
+const url = "http://18.233.226.58:8080";
 const globalApi = {
   register: `${url}/api/v1/register`,
   login: `${url}/api/v1/login`,
@@ -10,6 +9,7 @@ const globalApi = {
   eventDescription: `${url}/api/v1/event/descriptions/`, //+:eventId v2022.01
   createEvent: `${url}/api/v1/event/create`, //v2022.01
   userData: `${url}/api/v1/user/`, //+:userId
+  chatPartner: `${url}/api/v1/chat/partners/`,
 };
 
 export default globalApi;
