e496edeffaa9bb0b0b97021d31edeb420573c827
Suphalerk Boonvitchaikul <phet610@hotmail.com>
2022-03-29 23:17:21 +0700

    url

--- frontend/src/screens/EditProfile.js
+++ frontend/src/screens/EditProfile.js
@@ -421,7 +421,7 @@ function EditProfile() {
         <div style={{ display: "flex", flexDirection: "row" }}>
           <div className="empty" style={{ flex: 1 }} />
           <img
-            src={profileImg}
+            src={url(profileImg)}
             id="profile_img"
             style={{
               height: "500px",
