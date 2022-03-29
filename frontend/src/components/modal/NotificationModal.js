f3f0ba950198ea2b50a6ab82ca890a4e97bbc1d7
oJadeo <jade.jiravate@gmail.com>
2022-03-29 22:30:12 +0700

    OKAY

--- frontend/src/components/modal/NotificationModal.js
+++ frontend/src/components/modal/NotificationModal.js
@@ -1,186 +1,180 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // import Modal from 'react-bootstrap/Modal'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
+import React from "react";
+import ReactDOM from "react-dom";
+import Modal from "react-modal";
 
-import { Button } from 'react-bootstrap';
-import "./NotificationModal.css"
-import Cookies from 'universal-cookie';
-import globalApi from '../../globalApi';
+import { Button } from "react-bootstrap";
+import "./NotificationModal.css";
+import Cookies from "universal-cookie";
+import globalApi from "../../globalApi";
 
-import NotiBox from "./NotiBox"
+import NotiBox from "./NotiBox";
 
-import "../css_extensions/btn.css"
+import "../css_extensions/btn.css";
 
 var axios = require("axios").default;
 
-const NotificationModal=(props)=>{
+const NotificationModal = (props) => {
+  const cookies = new Cookies();
+  const user_cookie = cookies.get("cookie");
 
-    const cookies = new Cookies();
-    const user_cookie = cookies.get("cookie")
+  const isModalShow = props.notificationState;
+  const setNotificationModalState = props.setNotificationModalState;
 
-    const isModalShow = props.notificationState
-    const setNotificationModalState = props.setNotificationModalState
+  const [all_noti, setNoti] = useState([]);
 
-    const [all_noti,setNoti] = useState([])
+  const [noti_count, setNotiCount] = useState(0);
 
-    const [noti_count,setNotiCount] = useState(0)
-    
-    const hideModal=()=>{
-        console.log("clicked hide modal")
-        setNotificationModalState(false)
-    }
+  const hideModal = () => {
+    console.log("clicked hide modal");
+    setNotificationModalState(false);
+  };
 
-    const showModal=()=>{
-        setNotificationModalState(true)
-    }
+  const showModal = () => {
+    setNotificationModalState(true);
+  };
 
-    const toggleModal=()=>{
-        setNotificationModalState(!isModalShow)
-    }
+  const toggleModal = () => {
+    setNotificationModalState(!isModalShow);
+  };
 
-    // const customStyles = {
-    //     content: {
-    //     position: "fixed",
-    //     top: "20%",
-    //     right: "-10%",
-    //     width: "25%",
-    //     bottom: 'auto',
-    //     left: "auto",
-    //     marginRight: '0px',
-    //     transform: 'translate(-50%, -50%)',
-    //     "background-color": "#FAF3E7"
-    //     },
-    //   };
+  // const customStyles = {
+  //     content: {
+  //     position: "fixed",
+  //     top: "20%",
+  //     right: "-10%",
+  //     width: "25%",
+  //     bottom: 'auto',
+  //     left: "auto",
+  //     marginRight: '0px',
+  //     transform: 'translate(-50%, -50%)',
+  //     "background-color": "#FAF3E7"
+  //     },
+  //   };
 
-    const customStyles={
-        overlay: {
-          backgroundColor: "rgba(88, 88, 88, 0.1)",
-        },
-        content: {
-          width: "25%",
-          height: "auto",
+  const customStyles = {
+    overlay: {
+      backgroundColor: "rgba(88, 88, 88, 0.1)",
+    },
+    content: {
+      width: "25%",
+      height: "auto",
 
-          marginTop: "5vh",
-          marginLeft: "auto",
-          marginRight: "1vh",
+      marginTop: "5vh",
+      marginLeft: "auto",
+      marginRight: "1vh",
 
-          backgroundColor: "#FAF3E7",
-          overflow: "auto",
-          WebkitOverflowScrolling: "touch",
-          borderRadius: "20px",
-          outline: "none",
-          padding: "20px",
-        },
-      }
+      backgroundColor: "#FAF3E7",
+      overflow: "auto",
+      WebkitOverflowScrolling: "touch",
+      borderRadius: "20px",
+      outline: "none",
+      padding: "20px",
+    },
+  };
 
-      
+  // get noti count
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: globalApi.getNotiCount, // noti count endpoint
+      headers: {
+        Authorization: "Bearer " + user_cookie.accessToken,
+      },
+      timeout: 8000,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          console.log("ok herble");
+          console.log(res);
+          setNotiCount(res.data.count);
+        }
+      })
+      .catch((error) => {
+        console.log("error!!");
+        console.log(error);
+      });
+  }, []);
 
-    // get noti count 
-    useEffect(()=>{
-        axios({
-            method: 'get',
-            url: globalApi.getNotiCount, // noti count endpoint
-            headers: {
-                "Authorization" : "Bearer "+user_cookie.accessToken,
-            },
-            timeout: 8000
-        })
-        .then((res)=>{
-            if (res.status == 200) {
-                console.log("ok herble")
-                console.log(res)
-                setNotiCount(res.data.count)
-            }
-        })
-        .catch((error) => {
-            console.log("error!!")
-            console.log(error)
-        })
-    },[])
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: globalApi.getAllNoti,
+      headers: {
+        Authorization: "Bearer " + user_cookie.accessToken,
+      },
+      timeout: 8000,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          console.log("ok herble");
+          console.log(res);
+          setNoti(res.data.noti);
+        }
+      })
+      .catch((error) => {
+        console.log("error!!");
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(()=>{
-        axios({
-            method: 'get',
-            url: globalApi.getAllNoti,
-            headers: {
-                "Authorization" : "Bearer "+user_cookie.accessToken,
-            },
-            timeout: 8000
-        })
-        .then((res)=>{
-            if (res.status == 200) {
-                console.log("ok herble")
-                console.log(res)
-                setNoti(res.data.noti)
-            }
-        })
-        .catch((error) => {
-            console.log("error!!")
-            console.log(error)
-        })
-    },[])
+  // join event noti
 
-    // join event noti
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: globalApi.getAllNoti, //friend noti endpoint
+      headers: {
+        Authorization: "Bearer " + user_cookie.accessToken,
+      },
+      timeout: 8000,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          console.log("ok herble");
+          console.log(res);
+          setNoti(all_noti.concat(res.data.noti));
+        }
+      })
+      .catch((error) => {
+        console.log("error!!");
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(()=>{
-        axios({
-            method: 'get',
-            url: globalApi.getAllNoti, //friend noti endpoint
-            headers: {
-                "Authorization" : "Bearer "+user_cookie.accessToken,
-            },
-            timeout: 8000
-        })
-        .then((res)=>{
-            if (res.status == 200) {
-                console.log("ok herble")
-                console.log(res)
-                setNoti(all_noti.concat(res.data.noti))
-            }
-        })
-        .catch((error) => {
-            console.log("error!!")
-            console.log(error)
-        })
-    },[])
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: globalApi.getNotiCount,
+      headers: {
+        Authorization: "Bearer " + user_cookie.accessToken,
+      },
+      timeout: 8000,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          setNotiCount(res.data.count);
+        }
+      })
+      .catch((error) => {
+        console.log("error!!");
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(()=>{
-        axios({
-            method: 'get',
-            url: globalApi.getNotiCount,
-            headers: {
-                "Authorization" : "Bearer "+user_cookie.accessToken,
-            },
-            timeout: 8000
-        })
-        .then((res)=>{
-            if (res.status == 200) {
-                setNotiCount(res.data.count)
-            }
-        })
-        .catch((error) => {
-            console.log("error!!")
-            console.log(error)
-        })
-    },[])
+  // {Object.keys(this.props.events).length !== 0 &&
+  //     Object.keys(this.props.events).map((key, index) => (
+  //       <EventCard
+  //         key={this.props.events[key].eventId}
+  //         events={this.props.events[key]}
+  //       />
+  //     ))}
 
-    
-
-    // {Object.keys(this.props.events).length !== 0 &&
-    //     Object.keys(this.props.events).map((key, index) => (
-    //       <EventCard
-    //         key={this.props.events[key].eventId}
-    //         events={this.props.events[key]}
-    //       />
-    //     ))}
-
-
-    return (
-        <div>
-            {/* <Modal className='modal fade' id="noti-modal" animation={true} data-easein="bounce" show={isModalShow} onHide={hideModal}>
+  return (
+    <div>
+      {/* <Modal className='modal fade' id="noti-modal" animation={true} data-easein="bounce" show={isModalShow} onHide={hideModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Notification</Modal.Title>
                 </Modal.Header>
@@ -195,31 +189,63 @@ const NotificationModal=(props)=>{
                 </Modal.Footer>
             </Modal> */}
 
-            <Modal
-                closeTimeoutMS={200}
-                isOpen={isModalShow}
-                contentLabel="modal"
-                onRequestClose={() => hideModal}
-                style={customStyles}
+      <Modal
+        closeTimeoutMS={200}
+        isOpen={isModalShow}
+        contentLabel="modal"
+        onRequestClose={() => hideModal}
+        style={customStyles}
+      >
+        <div className="modal_header">
+          <h3>
+            Notifications{" "}
+            <Button
+              className="btn custom-button"
+              variant="info"
+              style={{
+                position: "relative",
+                float: "right",
+                top: "0",
+                "margin-top": "0",
+              }}
             >
-                <div className='modal_header'>
-                    <h3>Notifications <Button className='btn custom-button'variant="info" style={{"position": "relative", "float": "right", "top":"0", "margin-top":"0"}}>{noti_count}</Button></h3>
-                </div>
-                {all_noti.length > 0 && all_noti.map((noti)=>{
-                        return <NotiBox noti={noti}/>
-                })}
-                {all_noti.length <= 0 && <NotiBox noti={{
-                    "notiContent" : "No new Notifications",
-                    "url" : "/",
-                    "dateTime" : "2021-11-23 23:22:00"
-                }}
-                />}
-
-                <div style={{"position": "absolute", "bottom": "0", "right": "0", "margin": "10px"}}><Button className='btn custom-button'variant="success" onClick={hideModal}>Close</Button></div>
-            </Modal>
+              {noti_count}
+            </Button>
+          </h3>
         </div>
-    );
+        {all_noti.length > 0 &&
+          all_noti.map((noti) => {
+            return <NotiBox noti={noti} />;
+          })}
+        {all_noti.length <= 0 && (
+          <NotiBox
+            noti={{
+              notiContent: "No new Notifications",
+              url: "/",
+              dateTime: "2021-11-23 23:22:00",
+            }}
+          />
+        )}
 
-}
+        <div
+          style={{
+            position: "absolute",
+            bottom: "0",
+            right: "0",
+            margin: "10px",
+          }}
+        >
+          <Button
+            className="btn custom-button"
+            variant="success"
+            onClick={hideModal}
+          >
+            Close
+          </Button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
 export default NotificationModal;
