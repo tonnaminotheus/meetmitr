05ac173dadddf29291e53eca1fe7449f49525931
Suphalerk Boonvitchaikul <phet610@hotmail.com>
2022-04-26 21:20:15 +0700

    finished api connecting and redecorate

--- frontend/src/components/VerifyRequest.js
+++ frontend/src/components/VerifyRequest.js
@@ -23,8 +23,8 @@ const VerifyRequest=(props)=>{
     //get cookie
     const cookies = new Cookies()
     let user_cookie = cookies.get("cookie");
-    // let accessToken = user_cookie.accessToken;
-    let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxIiwiYXV0aG9yaXplZCI6dHJ1ZSwiZXhwIjoxNjUzNjI3NDc2fQ.9ZoVhVFMYxfweHRAu4B8J4naN7GMgRI69oP9gUpnbgg"
+    let accessToken = user_cookie.accessToken;
+    // let accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxIiwiYXV0aG9yaXplZCI6dHJ1ZSwiZXhwIjoxNjUzNjI3NDc2fQ.9ZoVhVFMYxfweHRAu4B8J4naN7GMgRI69oP9gUpnbgg"
 
     let navigate = useNavigate();
     const toLogin = () => {
@@ -56,30 +56,30 @@ const VerifyRequest=(props)=>{
     }
 
     //check isAdmin
-    // useEffect(()=>{
-    //     axios({
-    //         method: 'get',
-    //         url: globalApi.isAdmin,
-    //         headers: {
-    //             "Authorization" : "Bearer "+accessToken,
-    //         },
-    //         timeout: 8000
-    //     })
-    //     .then((res)=>{
-    //         if (res.status != 200) {
-    //             toLogin()
-    //         }
-    //         else {
-    //             setAdmin(true)
-    //             getRequests()
-    //         }
-    //     })
-    //     .catch(error => {
-    //         console.log("error!!")
-    //         console.log(error)
-    //     })
+    useEffect(()=>{
+        axios({
+            method: 'get',
+            url: globalApi.isAdmin,
+            headers: {
+                "Authorization" : "Bearer "+accessToken,
+            },
+            timeout: 8000
+        })
+        .then((res)=>{
+            if (res.status != 200) {
+                toLogin()
+            }
+            else {
+                setAdmin(true)
+                getRequests()
+            }
+        })
+        .catch(error => {
+            console.log("error!!")
+            console.log(error)
+        })
         
-    // },[]);
+    },[]);
 
     //getAllRequest
 
@@ -88,7 +88,10 @@ const VerifyRequest=(props)=>{
         <div className="backG">
             <div className="verify-page-container">
                 <h1>Hello!!!</h1>
-                <RequestComponent/>
+                {/* <RequestComponent/> */}
+                {requests.length > 0 && requests.map((req, index)=>{
+                  return <RequestComponent key={index} userData={req}/>
+                })}
                 {/* Stack */}
             </div>
         </div>
